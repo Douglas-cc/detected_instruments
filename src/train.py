18983c5ee435ed14f12293a4b53da1f0754a3de5
Douglas-cc <douglasdasilvasousa8@gmail.com>
2022-08-30 16:30:59 -0300

    add novas formas de seleção de features

--- src/train.py
+++ src/train.py
@@ -1,25 +1,32 @@
-SCRIPT_DIR = os.path.dirname('../src/')
-sys.path.append(os.path.dirname(SCRIPT_DIR))
-
+import numpy as np
+import pandas as pd
 from imblearn.over_sampling import SMOTE
+from sklearn.utils import  class_weight
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
 
 
-
-def cross_validate_smoth(k, model, X, y, over_sampling=False):
-    kfold = KFold(n_splits=k) 
+def cross_validate_balancead(k, model, X, y, oversampling=False, weight=False):
+    kfold =  StratifiedKFold(n_splits=k) 
     accuracy_split = []
     
     # interando sobre os splits
-    for idx, (idx_train, idx_validate) in enmerate(kfold.split(X)):
+    for idx, (idx_train, idx_validate) in enumerate(kfold.split(X, y)):
         X_split_train = X.iloc[idx_train, :]
         y_split_train = y.iloc[idx_train, :]
         
-        if over_sampling:
+        if oversampling:
             sm = SMOTE(random_state=42)
             X_split_train, y_split_train = sm.fit_resample(X_split_train, y_split_train)
             
+        if weight:
+            weights = class_weight.compute_class_weight(
+                class_weight = 'balanced',
+                classes = np.unique(y_split_train),
+                y = y_split_train.values.reshape(-1)
+            )
+            
+                    
         # com os dados balanceados SÓ NO TREINO, vamos treinar 
         model.fit(X_split_train, y_split_train.values.flatten())
         
@@ -28,10 +35,9 @@ def cross_validate_smoth(k, model, X, y, over_sampling=False):
         
         # validacao SEM oversampling, amostra do mundo real com dados desbalanceados
         predictions_val = model.predict(X_split_validate)
+        accuracy = accuracy_score(y_split_validate, predictions_val)
+        accuracy_split.append(accuracy)
         
-        accuracy_split = accuracy_score(y_split_validate, predictions_val)
-        
-        print(f'Acuracia do split {idx}: {accuracy_split}')
+        print(f'Acuracia do split {idx}: {accuracy}')
     
-    return accuracy_split
-        
+    return np.mean(accuracy_split)
