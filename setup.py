4fcba691c1d93d579e19e047e9938dcd57a1a7b3
Douglas-cc <douglasdasilvasousa8@gmail.com>
2022-09-09 18:20:20 -0300

    updates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scripts',
+    version='1.0',
+    author='Douglas',
+    author_email='douglas.datamining@gmail.com',
+    packages=find_packages()
+)
